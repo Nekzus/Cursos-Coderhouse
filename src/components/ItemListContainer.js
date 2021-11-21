0a6c360bb18f7f7a7021757b579642392d684e90
Nekzus <85507589+Nekzus@users.noreply.github.com>
2021-11-20 23:29:15 -0300

    Desafios 3 y 4

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,8 +1,12 @@
 const ItemListContainer = (props) => {
     const {mensaje} = props; // Destructuring de props.
+    const style={ // Estilo del mensaje.
+        'text-align': 'center',
+        'background-color': 'rgb(53, 51, 51)',
+    }
     return (
         <main>
-            <h2 className="mensaje-props">{mensaje}</h2>
+            <h2 className="mensaje-props" style={style}>{mensaje}</h2>
         </main>
     )
 }
